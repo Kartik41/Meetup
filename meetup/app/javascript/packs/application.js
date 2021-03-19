52c365023ce525dff9f827415ccfb81820280797
Kartik41 <kartik.chauhan@unthinkable.co>
2021-03-19 12:22:15 +0530

    changes

--- meetup/app/javascript/packs/application.js
+++ meetup/app/javascript/packs/application.js
@@ -20,7 +20,4 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-//= require nested_form_fields
-//= require jquery3
-//= require jquery_ujs
-//= require cocoon
+
--- meetup/config/routes.rb
+++ meetup/config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  
-  
   resources :friend_variants
   devise_for :users
   resources :friends
   root 'home#index'
   get 'home/about'
-  post 'friends/new'
-  post 'users/sign_in'
+ 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
